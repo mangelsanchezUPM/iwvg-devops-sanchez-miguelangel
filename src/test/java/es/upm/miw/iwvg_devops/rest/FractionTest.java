5d35dce4a91025102404ad5592b181412e7b5e13
Miguel Angel <miguelangelsanchezordaz@alumnos.upm.es>
2021-09-27 20:55:14 +0200

    added FractionTest # 13

--- src/test/java/es/upm/miw/iwvg_devops/rest/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/FractionTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class FractionTest {
     private Fraction fraction;
@@ -17,7 +17,9 @@ class FractionTest {
     }
 
     @Test
-    void testGetNumerator() { assertEquals(2, this.fraction.getNumerator()); }
+    void testGetNumerator() {
+        assertEquals(2, this.fraction.getNumerator());
+    }
 
     @Test
     void testSetNumerator() {
@@ -26,19 +28,57 @@ class FractionTest {
     }
 
     @Test
-    void testGetDenominator() { assertEquals(4,this.fraction.getDenominator()); }
+    void testGetDenominator() {
+        assertEquals(4, this.fraction.getDenominator());
+    }
 
     @Test
     void testSetDenominator() {
         this.fraction.setDenominator(5);
-        assertEquals(5,this.fraction.getDenominator());
+        assertEquals(5, this.fraction.getDenominator());
     }
 
     @Test
-    void testDecimal() {assertEquals(0.5, this.fraction.decimal());}
+    void testDecimal() {
+        assertEquals(0.5, this.fraction.decimal());
+    }
 
     @Test
     void testToString() {
         assertEquals("Fraction{numerator=2, denominator=4}", this.fraction.toString());
+    }
+
+    @Test
+    void testIsProper() {
+        assertTrue(this.fraction.isProper());
+    }
+
+    @Test
+    void testIsImproper() {
+        assertFalse(this.fraction.isImproper());
+    }
+
+    @Test
+    void testIsEquivalent() {
+        assertTrue(this.fraction.isEquivalent(new Fraction(4, 8)));
+        assertFalse(this.fraction.isEquivalent(new Fraction(5, 6)));
+    }
+
+    @Test
+    void testAddFraction() {
+        this.fraction.add(new Fraction(4, 16));
+        assertEquals(new Fraction(3,4) , this.fraction);
+    }
+
+    @Test
+    void testMultiplyFraction() {
+        this.fraction.multiply(new Fraction(4, 16));
+        assertEquals(new Fraction(1,8) , this.fraction);
+    }
+
+    @Test
+    void testDivideFraction() {
+        this.fraction.divide(new Fraction(4, 16));
+        assertEquals(new Fraction(2,1) , this.fraction);
     }
 }
